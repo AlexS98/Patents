9972b34ec51d9e31a724bc20784874a371bcf8cf
Alexander <sania.sviridenko@gmail.com>
2017-12-20 22:38:53 +0200

    add coverage

---
+++ Tests/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using Patents.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Xunit;
+
+namespace Tests
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void IndexTest()
+        {
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.NotNull(result);
+        }
+
+
+    }
+}
