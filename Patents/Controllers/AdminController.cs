4e61adc43c8793313d384be17e009967c2d7ff4e
Alexander <sania.sviridenko@gmail.com>
2017-12-29 01:00:04 +0200

    add admin panel

---
+++ Patents/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin.Security;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Patents.Controllers
+{
+    [Authorize(Roles = "Administrator,Moderator")]
+    public class AdminController : Controller
+    {
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            ViewBag.UserName = AuthenticationManager.User.Identity.Name.ToString();
+            return View();
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult AdminIndex()
+        {
+            ViewBag.UserName = AuthenticationManager.User.Identity.Name.ToString();
+            return View();
+        }
+    }
+}
